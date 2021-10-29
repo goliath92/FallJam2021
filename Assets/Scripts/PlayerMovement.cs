1245b4c2b4a1bf57c693b2e8696fe2adbfa25c1d
goliath92 <efeeray92@gmail.com>
2021-10-29 16:42:45 +0300

    Duvardan zıplama dahil.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,26 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
     private Animator anim;
-    private bool grounded;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCoolDown;
+    
+    
     
     [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
         
         // karakteri, gidilen yöne doğru çevirme
         if(horizontalInput>0.01f)
@@ -28,25 +34,66 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
         
-
-        if (Input.GetKey(KeyCode.Space) && grounded) 
-            Jump();
-        
         //animator kontrolü için
         anim.SetBool("Run",horizontalInput!=0);
+        
+        // Duvardan zıplamak için
+        if (wallJumpCoolDown > 0.2f)
+        {
+            
+            
+            body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
 
-    }
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+
+            else
+                body.gravityScale = 3;
+            
+            if (Input.GetKey(KeyCode.Space)) 
+                Jump();
+        }
+
+        else
+            wallJumpCoolDown += Time.deltaTime;
+
+        }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        grounded = false;
+        // yere temas durumunda
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+        }
+        
+        
+        //Duvara değdiğinde ama havada iken
+        else if (onWall()&&!isGrounded())
+        {
+            wallJumpCoolDown = 0;
+            body.velocity = new Vector2(-Math.Sign(transform.localScale.x) * 3, 6); 
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Ground")
-            grounded = true;
+       
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,Vector2.down,0.1f,groundLayer);
+        return raycastHit.collider != null;
+    }
+    
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,new Vector2(transform.localScale.x,0),0.1f,wallLayer);
+        return raycastHit.collider != null;
     }
 }
  
